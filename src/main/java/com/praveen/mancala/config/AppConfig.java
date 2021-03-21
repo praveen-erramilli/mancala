8b528679fdccef35d8c947e31d64c83942d5e556
Praveen <epraveenns@gmail.com>
2021-03-21 16:23:44 +0530

    Removed thread local games store

--- src/main/java/com/praveen/mancala/config/AppConfig.java
+++ src/main/java/com/praveen/mancala/config/AppConfig.java
@@ -1,66 +1,18 @@
 package com.praveen.mancala.config;
 
-import com.praveen.mancala.game.GameStore;
 import com.praveen.mancala.model.CustomPitSerializer;
-import com.praveen.mancala.model.Pit;
-import com.praveen.mancala.web.GameFilter;
-import org.springframework.aop.framework.ProxyFactoryBean;
-import org.springframework.aop.target.ThreadLocalTargetSource;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Scope;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import java.util.Collections;
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    public GameFilter gameFilter() {
-        return new GameFilter();
-    }
-
-    @Bean
-    public FilterRegistrationBean<GameFilter> tenantFilterRegistration() {
-        FilterRegistrationBean<GameFilter> result = new FilterRegistrationBean<>();
-        result.setFilter(this.gameFilter());
-        result.setUrlPatterns(Collections.singletonList("/*"));
-        result.setName("Game Store Filter");
-        result.setOrder(1);
-        return result;
-    }
-
-    @Bean
     public CustomPitSerializer customPitSerializer() {
         return new CustomPitSerializer();
     }
-
-    @Bean(destroyMethod = "destroy")
-    public ThreadLocalTargetSource threadLocalGameStore() {
-        ThreadLocalTargetSource result = new ThreadLocalTargetSource();
-        result.setTargetBeanName("gameStore");
-        return result;
-    }
-
-    @Primary
-    @Bean(name = "proxiedThreadLocalTargetSource")
-    public ProxyFactoryBean proxiedThreadLocalTargetSource(ThreadLocalTargetSource threadLocalTargetSource) {
-        ProxyFactoryBean result = new ProxyFactoryBean();
-        result.setTargetSource(threadLocalTargetSource);
-        return result;
-    }
-
-    @Bean(name = "gameStore")
-    @Scope(scopeName = "prototype")
-    public GameStore gameStore() {
-        return new GameStore();
-    }
-
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
--- src/main/java/com/praveen/mancala/controller/GameController.java
+++ src/main/java/com/praveen/mancala/controller/GameController.java
@@ -1,6 +1,5 @@
 package com.praveen.mancala.controller;
 
-import com.praveen.mancala.game.GameStore;
 import com.praveen.mancala.model.Game;
 import com.praveen.mancala.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,14 +10,10 @@ import org.springframework.web.bind.annotation.*;
 public class GameController {
 
     @Autowired
-    private GameStore gameStore;
-
-    @Autowired
     private GameService gameService;
 
     @GetMapping("/{id}")
     public Game getGame(@PathVariable(value = "id")  Long id) {
-        gameStore.setGameId(id);
         return gameService.getGame(id);
     }
 
@@ -29,7 +24,6 @@ public class GameController {
 
     @PutMapping("/{id}")
     public Game makeMove(@PathVariable("id") Long id, @RequestParam("pit_id") Long pitID) {
-        gameStore.setGameId(id);
         return gameService.makeMove(id, pitID);
     }
 }
